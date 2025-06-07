421f1199a351ca771eeda4d1e5c6e2f94b46cb74
Roman Gostilo <romanngostilo@gmail.com>
2025-06-07 18:03:06 +0300

    chore(krr): change interface analyzer

--- internal/analyzers/analyzer.go
+++ internal/analyzers/analyzer.go
@@ -2,8 +2,11 @@ package analyzers
 
 import (
 	"context"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type Analyzer interface {
 	Run(context.Context)
+	GetCollectors() *prometheus.Collector
 }

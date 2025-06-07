fc1ab00a390b8f68195db54e40def684d3895a37
Roman Gostilo <romanngostilo@gmail.com>
2025-06-07 18:49:56 +0300

    feat(cmd): add setting up analyzers in cmd

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"context"
+
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/vvvkkkggg/kubeconomist-core/internal/analyzers"
 	"github.com/vvvkkkggg/kubeconomist-core/internal/analyzers/krr"
 	"github.com/vvvkkkggg/kubeconomist-core/internal/billing"
@@ -8,25 +11,29 @@ import (
 )
 
 func Run() error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	cfg, err := config.Load("config/config.yaml")
 	if err != nil {
 		return err
 	}
 
-	var analyzerList []analyzers.Analyzer
-
-	_ = cfg
-
 	billing := billing.New()
 
-	collector, err := krr.NewCollector("krr")
-	if err != nil {
-		return err
+	analyzerList := []analyzers.Analyzer{
+		krr.NewKrrAnalyzer(billing, cfg.Analyzers.KRR),
 	}
 
-	krr.NewKrrAnalyzer(billing, collector)
+	var collectors []prometheus.Collector
 
-	panic("implement me")
+	for _, a := range analyzerList {
+		collectors = append(collectors, a.GetCollectors()...)
+	}
+
+	for _, a := range analyzerList {
+		go a.Run(ctx)
+	}
 
 	return nil
 }

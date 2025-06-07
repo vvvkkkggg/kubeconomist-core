9e608239422c6055c98ff796927212dab5b44e41
BaldiSlayer <lisov.a2005@yandex.ru>
2025-06-07 19:08:44 +0300

    update registry optimizer

--- internal/analyzers/registryoptimizer/registryoptimizer.go
+++ internal/analyzers/registryoptimizer/registryoptimizer.go
@@ -6,10 +6,12 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/vvvkkkggg/kubeconomist-core/internal/analyzers"
 	"github.com/vvvkkkggg/kubeconomist-core/internal/yandex"
+	"github.com/yandex-cloud/go-genproto/yandex/cloud/compute/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -18,15 +20,18 @@ import (
 var _ analyzers.Analyzer = &RegistryOptimizer{}
 
 type RegistryOptimizer struct {
+	Biling interface {
+		GetRegistryCost() (float64, error)
+	}
 }
 
-func getYandexImages(ctx context.Context, yandex *yandex.Client) (map[string]struct{}, error) {
+func getYandexImages(ctx context.Context, yandex *yandex.Client) (map[string]*compute.Image, error) {
 	clouds, err := yandex.GetClouds(ctx)
 	if err != nil {
 		log.Fatalf("Failed to get clouds: %v", err)
 	}
 
-	ycImages := make(map[string]struct{}, 0)
+	ycImages := make(map[string]*compute.Image, 0)
 	for _, cloud := range clouds {
 		folders, err := yandex.GetFolders(ctx, cloud.Id)
 		if err != nil {
@@ -40,7 +45,7 @@ func getYandexImages(ctx context.Context, yandex *yandex.Client) (map[string]str
 			}
 
 			for _, image := range images.Images {
-				ycImages[image.Name] = struct{}{}
+				ycImages[image.Name] = image
 			}
 		}
 	}
@@ -48,7 +53,7 @@ func getYandexImages(ctx context.Context, yandex *yandex.Client) (map[string]str
 	return ycImages, err
 }
 
-func getK8SImages(clientset *kubernetes.Clientset) (map[string]struct{}, error) {
+func getK8SImages(clientset *kubernetes.Clientset) ([]string, error) {
 	namespaces, err := clientset.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		panic(err.Error())
@@ -107,8 +112,46 @@ func (ro *RegistryOptimizer) Run(ctx context.Context) {
 
 	}
 
-	_ = ycImages
-	_ = k8sImages
+	registryCost, err := ro.Biling.GetRegistryCost()
+	if err != nil {
+
+	}
+
+	inUse := func(ycName string) bool {
+		for _, imageName := range k8sImages {
+			if strings.Contains(imageName, ycName) {
+				return true
+			}
+		}
+
+		return false
+	}
+
+	imagesToDelete := make([]string, 0, len(ycImages))
+
+	// если есть в Yandex Cloud, но нет в кубе
+	for name := range ycImages {
+		if inUse(name) {
+			imagesToDelete = append(imagesToDelete, name)
+		}
+	}
+
+	sumGB := 0.0
+
+	for _, name := range imagesToDelete {
+		if image, exists := ycImages[name]; exists {
+			sizeGB := float64(image.StorageSize) / (1024 * 1024 * 1024)
+			sumGB += sizeGB
+		}
+	}
+
+	// сколько сэкономим в час
+	totalCost := sumGB * registryCost
+
+	_ = totalCost
+
+	return
+
 }
 
 func (ro *RegistryOptimizer) GetCollectors() *prometheus.Collector {

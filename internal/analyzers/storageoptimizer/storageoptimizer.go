4b1c513b2028d11a68abd076d66c7ad55ccd4902
BaldiSlayer <lisov.a2005@yandex.ru>
2025-06-07 22:09:07 +0300

    chore(strorageoptimizer): some upd

--- internal/analyzers/storageoptimizer/storageoptimizer.go
+++ internal/analyzers/storageoptimizer/storageoptimizer.go
@@ -4,21 +4,51 @@ import (
 	"context"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/vvvkkkggg/kubeconomist-core/internal/billing"
 )
 
-const (
-	rpsThresholdHot = 200
-)
+type S3Client interface {
+	ListBuckets(ctx context.Context) ([]string, error)
+	GetBucketLocation(ctx context.Context, bucket string) (string, error)
+	GetBucketStorageClass(ctx context.Context, bucket string) (string, error)
+}
+
+type Billing interface {
+	GetObjectStoragePricesRUB(ctx context.Context) (*billing.ObjectStoragePrices, error)
+}
 
 type StorageOptimizer struct {
+	profitGauge *prometheus.GaugeVec
+	s3Client    S3Client
+	billing     Billing
 }
 
 func New() *StorageOptimizer {
-	return &StorageOptimizer{}
+	profitGauge := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "s3_bucket_optimization_profit",
+			Help: "Profit from optimizing S3 bucket storage class",
+		},
+		[]string{"bucket"},
+	)
+
+	return &StorageOptimizer{
+		profitGauge: profitGauge,
+	}
 }
 
 func (so *StorageOptimizer) Run(ctx context.Context) {
+	_, err := so.billing.GetObjectStoragePricesRUB(ctx)
 
+	buckets, err := so.s3Client.ListBuckets(ctx)
+	if err != nil {
+		// Обработка ошибки
+		return
+	}
+
+	for _, bucket := range buckets {
+		_ = bucket
+	}
 }
 
 func (so *StorageOptimizer) GetCollectors() []prometheus.Collector {

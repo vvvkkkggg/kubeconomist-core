bc533460eea63ff707d3223bb05c459026ffbc23
a1k0u <alko7enko@gmail.com>
2025-06-08 17:55:40 +0300

    chore: update gitignore

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,9 +3,3 @@ variable "folder_id" {}
 variable "zone" {
   default = "ru-central1-a"
 }
-variable "access_key" {}
-variable "secret_key" {}
-variable "network_id" {}
-variable "subnet_id" {}
-variable "k8s_sa_id" {}
-variable "k8s_node_sa_id" {}

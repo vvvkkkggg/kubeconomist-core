2a08ac2876930e35b39409f31f1442390c29077a
a1k0u <alko7enko@gmail.com>
2025-06-08 17:33:13 +0300

    chore: add tests dns and ips

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,13 +1,15 @@
 resource "yandex_vpc_address" "static_ip" {
-  name = "alkosenko-static-ip"
+  count = 3
+  name  = "alkosenko-static-ip-${count.index + 1}"
   external_ipv4_address {
     zone_id = var.zone
   }
 }
 
 resource "yandex_dns_zone" "zone" {
-  name        = "alkosenko-test"
-  zone        = "alkosenko.test."
+  count       = 5
+  name        = "alkosenko-test-${count.index + 1}"
+  zone        = "alkosenko${count.index + 1}.test."
   public      = true
-  description = "Demo domain zone"
+  description = "Demo domain zone ${count.index + 1}"
 }

0d1c91a6cde9207ad5fa385d80c409cdc53c02e5
a1k0u <alko7enko@gmail.com>
2025-06-08 17:33:27 +0300

    chore: add tests s3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,20 @@
 resource "yandex_storage_bucket" "bucket" {
-  bucket     = "alkosenko-test"
-  access_key = var.access_key
-  secret_key = var.secret_key
-  acl        = "private"
+  count  = 3
+  bucket = "alkosenko-test-${count.index + 1}"
+  acl    = "private"
+}
+
+resource "null_resource" "generate_file" {
+  count = 3
+  provisioner "local-exec" {
+    command = "head -c 104857600 </dev/urandom > ./dummy_100mb_${count.index + 1}.bin"
+  }
+}
+
+resource "yandex_storage_object" "cute-cat-picture" {
+  count = 3
+
+  bucket = "alkosenko-test-${count.index + 1}"
+  key    = "test-${count.index + 1}"
+  source = "./dummy_100mb_${count.index + 1}.bin"
 }
